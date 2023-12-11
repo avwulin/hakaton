0af4f1a632f7d4e6517d0db86cffc43fde2143b0
Polina <polinamordvinceva266@gmail.com>
2023-12-11 15:15:30 +0700

    Score

--- Assets/Hakaton/Scripts/DamageSystem.cs
+++ Assets/Hakaton/Scripts/DamageSystem.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DamageSystem : MonoBehaviour
 {
-
     public int MaxHealth;
     public int Health;
     public int Damage;
+    public int Score; // Новая переменная для счетчика очков
+
+    public Text scoreText; // Ссылка на текстовое поле для отображения счета
 
     private void Start()
     {
         Health = MaxHealth;
+        Score = 0; // Инициализация счетчика очков
+        UpdateScoreUI(); // Обновление UI счетчика очков
     }
 
+    private void UpdateScoreUI()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + Score.ToString();
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -21,12 +33,21 @@ public class DamageSystem : MonoBehaviour
         {
             Health -= Damage;
             float healthNormalized = (float)Health / MaxHealth;
-            transform.GetComponent<Renderer>().material.color = Color.Lerp(Color.black, Color.white, healthNormalized);
+            Renderer renderer = GetComponent<Renderer>();
+
+            if (renderer != null)
+            {
+                renderer.material.color = Color.Lerp(Color.black, Color.white, healthNormalized);
+            }
+
             Destroy(collision.gameObject);
 
             if (Health <= 0)
             {
+                // Уничтожение объекта и добавление очков
                 Destroy(gameObject);
+                Score += 100;
+                UpdateScoreUI();
             }
         }
     }
